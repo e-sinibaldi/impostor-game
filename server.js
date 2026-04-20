5084e1a9a1821cd134a0fb50e7c33c160adb8fe6
e-sinibaldi <ernestosinibaldi@gmail.com>
2026-04-20 20:04:28 -0300

    Add sticky sessions and health check for multi-machine deploy

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cookie: true
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
