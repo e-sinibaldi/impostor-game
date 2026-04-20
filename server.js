7d863fa5b67ed3909c1e81b6c40488949133391b
e-sinibaldi <ernestosinibaldi@gmail.com>
2026-04-20 19:59:04 -0300

    Fix catch-all route: use app.use instead of app.get(*)

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
